9fc3cf254057ae94e37567c22cd113a28dc004ce
Elliodev <elliodevv@gmail.com>
2026-03-27 16:35:45 +0100

    feat: implement core todo logic with signals and zen design

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,48 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, signal } from '@angular/core';
+
+export interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('zen-todo');
+
+  todoText = signal('');
+  todos = signal<Todo[]>([]);
+
+  remainingCount = computed(() => {
+    return this.todos().filter((t) => !t.completed).length;
+  });
+
+  addTodo() {
+    // .trim() pour ignorer les espaces inutiles
+    if (this.todoText().trim() != '') {
+      let todoDetails: Todo = {
+        id: Date.now(),
+        text: this.todoText(),
+        completed: false,
+      };
+      this.todos.update((a) => [...a, todoDetails]);
+
+      this.todoText.set('');
+    }
+  }
+
+  toggleTodo(id: number) {
+    this.todos.update((liste) =>
+      liste.map((t) => (t.id === id ? { ...t, completed: !t.completed } : t)),
+    );
+  }
+
+  removeTodo(id: number) {
+    this.todos.update((liste) => liste.filter((t) => t.id !== id));
+  }
 }
